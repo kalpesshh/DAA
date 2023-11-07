fa4aab791e7e20f4492382aa3c8410426e23cd86
kalpesshh <91608093+kalpesshh@users.noreply.github.com>
2023-11-08 01:54:05 +0530

    Add files via upload

---
+++ mergesort.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+
+// Merge two subarrays of the original array
+void merge(std::vector<int>& arr, int left, int middle, int right) {
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    std::vector<int> leftArray(n1);
+    std::vector<int> rightArray(n2);
+
+    for (int i = 0; i < n1; i++) {
+        leftArray[i] = arr[left + i];
+    }
+
+    for (int j = 0; j < n2; j++) {
+        rightArray[j] = arr[middle + 1 + j];
+    }
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (leftArray[i] <= rightArray[j]) {
+            arr[k] = leftArray[i];
+            i++;
+        } else {
+            arr[k] = rightArray[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = leftArray[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = rightArray[j];
+        j++;
+        k++;
+    }
+}
+
+// Merge Sort function
+void mergeSort(std::vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int middle = left + (right - left) / 2;
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+        merge(arr, left, middle, right);
+    }
+}
+
+int main() {
+    std::vector<int> arr = {12, 11, 13, 5, 6, 7};
+    int arrSize = arr.size();
+
+    mergeSort(arr, 0, arrSize - 1);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < arrSize; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
